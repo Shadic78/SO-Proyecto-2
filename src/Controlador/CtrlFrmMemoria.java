7efbcd04744cec6be767ba70f706cd66257d2066
Shadic78 <carlosjosue7889@gmail.com>
2020-03-14 18:49:04 -0600

    Insertar proceso siguiente

--- src/Controlador/CtrlFrmMemoria.java
+++ src/Controlador/CtrlFrmMemoria.java
@@ -26,11 +26,19 @@ public class CtrlFrmMemoria {
                 insertarProcesoEnMemoria();
             }
         }
+        else {
+            if(hayProcesosEnMemoria()) {
+                retirarSiguienteProceso();
+            }
+            else {
+                System.out.println("El programa termino");
+            }
+        }
         System.out.println("\n");        
-        System.out.println(Main.areasLibres);
-        System.out.println(Main.particiones);
-        System.out.println(Main.colaProcesos);
-        System.out.println(Main.colaAuxiliarProcesos);
+        System.out.println("Libres: " + Main.areasLibres);
+        System.out.println("Particiones: " + Main.particiones);
+        System.out.println("Procesos: " + Main.colaProcesos);
+        System.out.println("Aux: " + Main.colaAuxiliarProcesos);
         System.out.println("\n");
     }
     
@@ -70,6 +78,7 @@ public class CtrlFrmMemoria {
                 celda.setProceso(proceso);
                 celda.setSize(proceso.getSize());
                 celda.setDisponible(false);
+                System.out.println("Se inserto: " + proceso.getNombre());                
                 Main.areasLibres.remove(i);
                 Main.particiones.add(celda);
                 Main.colaProcesos.remove(0);
@@ -88,6 +97,40 @@ public class CtrlFrmMemoria {
         System.out.println("No se pudo insertar " + proceso.getNombre());
         Main.colaAuxiliarProcesos.add(proceso);
         Main.colaProcesos.remove(0);
+    }
+    
+    private boolean hayProcesosEnMemoria() {
+        boolean existeProceso = false;
+        if(Main.particiones.size() > 0) {
+            existeProceso = true;
+        }
+        return existeProceso;
+    }
+    
+    private void retirarSiguienteProceso() {
+        System.out.println("Retirar proceso siguiente");
+        // Encontrar el menor
+        Proceso proceso = Main.particiones.get(0).getProceso();
+        int celdaARetirar = -1;
+        int finMenor = proceso.getLlegada() + proceso.getDuracion();
+        int fin2 = 0;
+        for(int i = 0; i < Main.particiones.size(); i++) {
+            proceso = Main.particiones.get(i).getProceso();
+            fin2 = proceso.getLlegada() + proceso.getDuracion();
+            if(fin2 <= finMenor) {
+                finMenor = fin2;
+                celdaARetirar = i;
+            }
+        }
+        
+        if(celdaARetirar >= 0) {
+            System.out.println("Se retiro: " + Main.particiones.get(celdaARetirar).getProceso().getNombre());
+            CeldaMemoria celdaDesocupada = Main.particiones.get(celdaARetirar);
+            celdaDesocupada.setProceso(null);
+            celdaDesocupada.setDisponible(true);
+            Main.areasLibres.add(celdaDesocupada);
+            Main.particiones.remove(celdaARetirar);
+        }
     }
 
     public FrmMemoria getForm() {
